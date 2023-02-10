3e428cbd164daf0b0f8d1f1574cef319d9045c06
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-02-09 21:32:27 -0500

    HTML buttons

---
+++ main.js
@@ -0,0 +1,20 @@
+let a = 5;
+let b = 5;
+
+function add() {
+    return a + b;
+}
+
+function sub() {
+    return a - b;
+}
+
+function mult() {
+    return a * b;
+}
+
+function div() {
+    return a / b;
+}
+
+function operate(operator, a , b);
