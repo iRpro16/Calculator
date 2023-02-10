bdb58b9d11e07462e4b7fd0733c35662eeeb4e57
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-02-10 18:16:20 -0500

    Fetched a & b values, and got UI

--- main.js
+++ main.js
@@ -1,23 +1,69 @@
-let a = 0;
-let b = 0;
+let a = "";
+let b = "";
 let operator = "";
+let currentNum = document.querySelector('.current-num');
+let previousNum = document.querySelector('.previous-num');
+let buttonNums = document.querySelectorAll('.number');
+let buttonOps = document.querySelectorAll('.operator');
 
-function add() {
-    return a + b;
+
+// Function to populate and retrieve a & b values:
+buttonNums.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if (operator === "") {
+            previousNum.textContent += e.target.value;
+            a = previousNum.textContent;
+            console.log(`a is ${a}`);
+        } else {
+            currentNum.textContent += e.target.value;
+            b = currentNum.textContent;
+            console.log(`b is ${b}`);
+        }
+    })
+})
+
+// Function to get operator and call operate function:
+buttonOps.forEach(op => {
+    op.addEventListener('click', (e) => {
+        operator = e.target.textContent;
+        if (operator === "=") operate(operator, a, b);
+    })
+});
+
+// Function to add:
+function add(a, b) {
+    console.log(a + b);
 }
 
+// Function to subtract:
 function sub() {
     return a - b;
 }
 
+
+// Function to multiply:
 function mult() {
     return a * b;
 }
 
+// Function to divide:
 function div() {
     return a / b;
 }
 
-function operate(operator, a , b) {
-
+// Function to operate depending on case:
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+": return add(a, b);
+        case "-": return sub();
+        case "*": return mult();
+        case "÷": return div();
+    }
 };
+
+
+/*
+previousNum.textContent += e.target.value;
+a = previousNum.textContent;
+console.log(a);
+*/
