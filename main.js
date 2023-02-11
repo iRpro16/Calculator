5281d9d07449d47aa6aa0eeae12ee5ac1c365979
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-02-10 19:07:39 -0500

    Created clear function

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ let currentNum = document.querySelector('.current-num');
 let previousNum = document.querySelector('.previous-num');
 let buttonNums = document.querySelectorAll('.number');
 let buttonOps = document.querySelectorAll('.operator');
+let buttonClear = document.querySelector('.clear');
 
+// On click, clear:
+buttonClear.onclick = clear;
 
 // Function to populate and retrieve a & b values:
 buttonNums.forEach(number => {
@@ -25,45 +28,46 @@ buttonNums.forEach(number => {
 // Function to get operator and call operate function:
 buttonOps.forEach(op => {
     op.addEventListener('click', (e) => {
-        operator = e.target.textContent;
-        if (operator === "=") operate(operator, a, b);
+        if (e.target.textContent !== "=") {
+            operator = e.target.innertext;
+        } else {
+            operate();
+        }
     })
 });
 
 // Function to add:
 function add(a, b) {
-    console.log(a + b);
+    previousNum.textContent = "";
+    currentNum.textContent = parseInt(a) + parseInt(b);
 }
 
 // Function to subtract:
-function sub() {
-    return a - b;
+function sub(a, b) {
+    previousNum.textContent = "";
+    currentNum.textContent = parseInt(a) - parseInt(b);
 }
 
-
 // Function to multiply:
-function mult() {
-    return a * b;
+function mult(a, b) {
+    previousNum.textContent = "";
+    currentNum.textContent = parseInt(a) * parseInt(b);
 }
 
 // Function to divide:
-function div() {
-    return a / b;
+function div(a, b) {
+    previousNum.textContent = "";
+    curentNum.textContent = parseInt(a) / parseInt(b);
 }
 
 // Function to operate depending on case:
 function operate(operator, a, b) {
-    switch (operator) {
-        case "+": return add(a, b);
-        case "-": return sub();
-        case "*": return mult();
-        case "÷": return div();
-    }
+    if (operator === "+") add(a,b);
 };
 
+// Function clear:
 
-/*
-previousNum.textContent += e.target.value;
-a = previousNum.textContent;
-console.log(a);
-*/
+function clear() {
+    previousNum.textContent = "";
+    currentNum.textContent = "";
+}
