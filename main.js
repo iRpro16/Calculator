ff79aac53b3d71af2af4bb986e96f046aafd39b5
Isidoro Rumeo <rumeoisidoro16@gmail.com>
2023-02-12 19:44:56 -0500

    New functions, changed logic.

--- main.js
+++ main.js
@@ -1,89 +1,74 @@
 let a = "";
 let b = "";
 let operator = "";
-let currentNum = document.querySelector('.current-num');
-let previousNum = document.querySelector('.previous-num');
-let buttonNums = document.querySelectorAll('.number');
-let buttonOps = document.querySelectorAll('.operator');
-let buttonClear = document.querySelector('.clear');
-let equalBtn = document.querySelector('.op-equal');
+const currentNum = document.querySelector('.current-num');
+const previousNum = document.querySelector('.previous-num');
+const buttonNums = document.querySelectorAll('.number');
+const buttonOps = document.querySelectorAll('.operator');
+const buttonClear = document.querySelector('.clear');
+const equalBtn = document.querySelector('.op-equal');
 
-// On click, calculate: 
-equalBtn.addEventListener('click', () => {
-    operate(operator, a, b);
-});
-
-// On click, clear:
-buttonClear.onclick = clear;
-
-// Function to populate and retrieve a & b values:
-buttonNums.forEach(number => {
-    number.addEventListener('click', (e) => {
-        // Clear the 0:
-        if (currentNum.textContent == 0) currentNum.textContent = "";
-        if (operator === "") {
-            previousNum.textContent += e.target.value;
-            a = previousNum.textContent;
-            console.log(`a is ${a}`);
-        } else {
-            currentNum.textContent += e.target.value;
-            b = currentNum.textContent;
-            console.log(`b is ${b}`);
-        }
-    })
-});
-
-// Function to get operator and call operate function:
-buttonOps.forEach(op => {
-    op.addEventListener('click', (e) => {
-            operator = e.target.textContent;
-            //previousNum.textContent += operator;
-        
-    })
-});
-
-// Function to add:
-function add(a, b) {
-    previousNum.textContent = "";
-    currentNum.textContent = parseFloat(a) + parseFloat(b);
-}
-
-// Function to subtract:
-function sub(a, b) {
-    previousNum.textContent = "";
-    currentNum.textContent = parseFloat(a) - parseFloat(b);
-}
-
-// Function to multiply:
-function mult(a, b) {
-    previousNum.textContent = "";
-    currentNum.textContent = parseFloat(a) * parseFloat(b);
-}
-
-// Function to divide:
-function div(a, b) {
-    previousNum.textContent = "";
-    currentNum.textContent = parseFloat(a) / parseFloat(b);
-}
-
-// Function to operate depending on case:
-function operate(operator, a, b) {
+function calculate() {
+    let previous = parseFloat(a);
+    let current = parseFloat(b);
+    let result;
+    
     switch(operator) {
-        case "+": add(a, b);
+        case "+": result = previous + current;
         break;
-        case "-": sub(a, b);
+        case "-": result = previous - current;
         break;
-        case 'x': mult(a, b);
+        case "x": result = previous * current;
         break;
-        case "÷": div (a, b);
-        break;
+        case "÷": result = previous / current;
     }
+
+    b = result;
+    a = "";
+    operator = "";
 };
 
+buttonNums.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (b == 0) b = '';
+        if (button.innerHTML == "." && b.includes('.')) return;
+        b += (button.textContent);
+        refresh();
+    })
+});
 
-// Function clear:
-function clear() {
-    previousNum.textContent = "";
-    currentNum.textContent = "";
+buttonOps.forEach((button) => {
+    button.addEventListener('click', () => {
+        operate();
+        operator = (button.innerHTML);
+        b = "";
+        refresh();
+    })
+})
+
+equalBtn.addEventListener('click', () => {
+    calculate();
+    refresh();
+});
+
+buttonClear.addEventListener('click', () => {
+    b = 0;
+    a = ''; 
     operator = "";
+    refresh();
+});
+
+function operate() {
+    if (b == "") return;
+
+    if (a !== "" && operator !== "") {
+        calculate();
+    }
+    a = b + operator;
+    b = '';
+}
+
+function refresh() {
+    currentNum.textContent = b;
+    previousNum.textContent = a;
 }
